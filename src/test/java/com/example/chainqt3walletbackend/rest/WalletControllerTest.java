916136ad582777a6f1953f01a353083c9bb63d22
KQT3 <your_donotreply_email_id>
2022-07-23 22:51:47 +0200

    Stuff

--- src/test/java/com/example/chainqt3walletbackend/rest/WalletControllerTest.java
+++ src/test/java/com/example/chainqt3walletbackend/rest/WalletControllerTest.java
@@ -4,7 +4,6 @@ import com.example.chainqt3walletbackend.entity.DTOs.AccountDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -15,13 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WalletControllerTest {
-
     @Autowired
     WebTestClient webTestClient;
 
     @Test
     void newAccountSuccess() {
-
         webTestClient
                 .get().uri("/api/new-account")
                 .accept(MediaType.APPLICATION_JSON)
